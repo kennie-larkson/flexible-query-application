67800e1f4ed441eec244aeae0c8963bf7d095c45
kennie-larkson <materialkenny@gmail.com>
2024-07-31 10:53:55 +0100

    fixed port variable

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get<ConfigService>(ConfigService);
-  const port = configService.get('PORT');
+  const port = configService.get('PORT') || 9000;
 
   //app.useGlobalPipes(new ValidationPipe({}));
   const config = new DocumentBuilder()
